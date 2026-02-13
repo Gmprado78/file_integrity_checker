2317009c041116821336ec88ff1199be9e0fc075
Andrew McCullough <mcculloughandrew572@gmail.com>
2026-02-13 16:32:12 -0500

    Added Shell Gooey, time delay, spacing with shell Gooey. Pre-ANSI color add

--- file_intergrity_checker.py
+++ file_intergrity_checker.py
@@ -1,4 +1,8 @@
 import hashlib
+
+# Create a delay to make the shell Gooey look better(It'll seem like its "computing" more)
+import time
+
 def get_hash(filename):
     file = open(filename, "rb")
     data = file.read()
@@ -6,31 +10,74 @@ def get_hash(filename):
     return hashlib.sha256(data).hexdigest()
 
 
-# Get the name of the file you are trying to check
-filename = input("Enter the name of the file you want to check: ")
-
-# Tells python which file to look at(adress)
-stored_files_hashes = filename + ".integrity" 
-
-
-# Calls get_hash function and reads and gets hash and stores as a stirng in strating_hash 
-starting_hash = get_hash(filename)
-
 
 # Need try becuase it may not work if its the first time running the program(No stored hash)
-try:
-    
-    # If can't open/read, Raise a FileNotFoundError
-    hash_value = open(stored_files_hashes, "r") # opens the stored hash in the read mode(Don't need to read the bytes("rb"))
-    stored_hash = hash_value.read() # Reads the stored hash value and stores it as a string in stored_hash
-    hash_value.close()
+def check_file_integrity(filename):  
+    try:
+        # If can't open/read, Raise a FileNotFoundError
+        hash_value = open(stored_files_hashes, "r") # opens the stored hash in the read mode(Don't need to read the bytes("rb"))
+        stored_hash = hash_value.read() # Reads the stored hash value and stores it as a string in stored_hash
+        hash_value.close()
 
-    if stored_hash == starting_hash:
-        print("File integrity is confirmed. The file has not been modified.")
+        if stored_hash == starting_hash:
+            print(f"File integrity of {filename} file is confirmed. The file has not been modified.")
+            print()
+            print("-------------------------------------------------------------------------------------------------")
+            print()
+        else:
+            print(f"THE FILE {filename} HAS BEEN MODIFIED. THE HASH VALUES DO NOT MATCH.")
+            print()
+            print("-------------------------------------------------------------------------------------------------")
+            print()
+    except FileNotFoundError: # Happens when the new .integrity file of a files hash does not exist yet.
+        print("File does not have a hash, create one")
+        print()
+        print("-------------------------------------------------------------------------------------------------")
+        print()
+
+print("---------------------------------------------File Integrity Checker---------------------------------------------")
+
+while True:
+    print("1) Create a file hash\n")
+    print("2) Check file integrity\n")
+    print("3) Exit\n")
+    user_input = input("Enter your choice: ")
+
+    if user_input == "1":
+        time.sleep(2)
+    # Get the name of the file you are trying to check
+        filename = input("Enter the name of the file you want to check: ")
+
+    # Tells python which file to look at(adress)
+        stored_files_hashes = "hashed_files/" + filename + ".integrity" 
+
+        # Calls get_hash function and reads and gets hash and stores as a string in strating_hash 
+        starting_hash = get_hash(filename)
+        
+        hash_value = open(stored_files_hashes, "w") # Opens the .integrity file in write mode to create it and write the hash value to it beucase Read does not create a file. 
+        hash_value.write(starting_hash) # The current hash value of the file is added to the .integrity file
+        hash_value.close()
+        print(f"Hash value created for {filename} and stored successfully.")
+        print()
+        print("-------------------------------------------------------------------------------------------------")
+        print()
+
+    elif user_input == "2":
+        time.sleep(2)
+        # Creates new input so the user can check the integrity of multiple files
+        file_check = input("Enter the name of the file you want to check: ")
+        filename = file_check
+        check_file_integrity(filename)
+    elif user_input == "3":
+        decision = input("Are you sure you want to exit? (y/n): ")
+        if decision == "n":
+            print()
+            print("-------------------------------------------------------------------------------------------------")
+            print()
+            continue
+        elif decision == "y":
+            print("Exiting the program.")
+            break
     else:
-        print("THE FILE HAS BEEN MODIFIED. THE HASH VALUES DO NOT MATCH.")
-except FileNotFoundError: # Happens when the new .integrity file of a files hash does not exist yet.
-    hash_value = open(stored_files_hashes, "w") # Opens the .integrity file in write mode to create it and write the hash value to it beucase Read does not create a file. 
-    hash_value.write(starting_hash) # The current hash value of the file is added to the .integrity file
-    hash_value.close()
-    print("First time running the program. The hash value of the given file is now stored for future checks")
+        print("Invalid input. Select 1, 2, or 3.")
+
